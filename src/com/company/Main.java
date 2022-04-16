448ae4dee8c7ea0dc157e08e6a16bf4de5e0efc1
Sergey Filippov <filippovsergeym@gmail.com>
2022-04-16 20:07:02 +0300

    review code...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Play desk = new Play();
         desk.initDesk();
         while(true){
@@ -15,31 +13,19 @@ public class Main {
             desk.draw();
             desk.checkWin("x");
         }
-
     }
 }
 
 class Play {
-
     private static final int max_desk = 3;
-
     private final String[][] desk = new String[max_desk][max_desk];
-//    private final int[][] brain = new int[max_desk][max_desk];
-
     public void initDesk(){
         for(int x = 0; x<max_desk; x++){
             for(int y = 0; y<max_desk; y++) {
                 desk[x][y] = " ";
             }
         }
-
-//        brain[0][0] = 1;
-//        brain[0][2] = 1;
-//        brain[2][0] = 1;
-//        brain[2][2] = 1;
-//        brain[1][1] = 2;
     }
-
     private static int toInt(String str){
         int a = -1;
         try {
@@ -50,7 +36,6 @@ class Play {
         if ((a < max_desk) & (a >= 0)) return a;
         else return -1;
     }
-
     public void getHop() {
         int x,y;
         while (true) {
@@ -71,7 +56,6 @@ class Play {
         }
         desk[x][y] = "x";
     }
-
     public void goAI(){
         int max=2;
         int min=0;
@@ -82,17 +66,14 @@ class Play {
         } while (!desk[x][y].equals(" "));
         desk[x][y] = "0";
     }
-
     private void driveLine(){
         for(int i=0; i<max_desk;i++){
             System.out.print("+-");
         }
         System.out.println("+");
     }
-
     public void draw(){
         for (int x=0; x < max_desk; x++ ){
-            //System.out.println("+-+-+-+");
             driveLine();
             for (int y=0; y < max_desk; y++ ){
                 System.out.print("|");
@@ -100,10 +81,8 @@ class Play {
             }
             System.out.println("|");
         }
-        //System.out.println("+-+-+-+");
         driveLine();
     }
-
     public void checkWin(String simvol){
         int x,y;
         // on horizontal
